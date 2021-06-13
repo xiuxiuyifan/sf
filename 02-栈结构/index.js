42a310e02d3e1811a9032324e4048241249babfa
xiuxiuyifan <xiuxiuyifan@163.com>
2021-06-13 18:41:09 +0800

    first commit

---
+++ 02-栈结构/index.js
@@ -0,0 +1,29 @@
+const Stack = require('./stack')
+
+var stack = new Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+
+// console.log(stack)               //Stack { items: [ 1, 2, 3, 4, 5 ] }
+
+// stack.pop()
+
+// console.log(stack)              //Stack { items: [ 1, 2, 3, 4 ] }
+
+// console.log(stack.peek())       //4
+
+// console.log(stack.isEmpty())    //false
+
+// console.log(stack.size())       //4
+
+// console.log(stack.print())      //1 2 3 4
+
+
+//栈的应用 
+
+
+//十进制转二进制
---
+++ 02-栈结构/stack.js
@@ -0,0 +1,44 @@
+//采用Es5  的写法来写。
+
+function Stack() {
+  //用来存储数据
+  this.items = []
+
+
+  //定义栈的基本操作
+
+  //1. 把元素压入栈中
+  Stack.prototype.push = function (element) {
+    this.items.push(element)
+  }
+  //2. 从栈中取出元素
+  Stack.prototype.pop = function () {
+    return this.items.pop()
+  }
+
+  //3. 查看一下栈顶元素
+  Stack.prototype.peek = function () {
+    return this.items[this.items.length - 1]
+  }
+
+  //4. 判断栈是否为空
+  Stack.prototype.isEmpty = function () {
+    return this.items === 0
+  }
+
+  //5. 获取栈中的元素个数
+  Stack.prototype.size = function () {
+    return this.items.length
+  }
+
+  //6. 查看当前栈里面的内容
+
+  Stack.prototype.print = function () {
+    return this.items.join(' ')
+  }
+}
+
+
+module.exports = Stack
+
+
