2be3d16c616f128bce01a75f4317b1545644c766
xiuxiuyifan <xiuxiuyifan@163.com>
2021-06-13 19:51:15 +0800

    栈练习代码完成

--- 02-栈结构/index.js
+++ 02-栈结构/index.js
@@ -1,12 +1,12 @@
 const Stack = require('./stack')
 
-var stack = new Stack()
+// var stack = new Stack()
 
-stack.push(1)
-stack.push(2)
-stack.push(3)
-stack.push(4)
-stack.push(5)
+// stack.push(1)
+// stack.push(2)
+// stack.push(3)
+// stack.push(4)
+// stack.push(5)
 
 // console.log(stack)               //Stack { items: [ 1, 2, 3, 4, 5 ] }
 
@@ -27,3 +27,33 @@ stack.push(5)
 
 
 //十进制转二进制
+
+// 10  5 0 
+
+// 5   2 1
+
+// 2   1 0
+
+// 1   0 1
+
+// 1010
+
+function dec2bin(dec) {
+  let stack = new Stack()
+
+  while (dec > 0) {
+    //进栈
+    stack.push(dec % 2)
+    dec = Math.floor(dec / 2)
+  }
+  let bin = ""
+  while (!stack.isEmpty()) {
+    bin += stack.pop();
+  }
+  return bin
+}
+
+let bin = dec2bin(10)
+
+console.log(bin)
+
--- 02-栈结构/stack.js
+++ 02-栈结构/stack.js
@@ -3,39 +3,38 @@
 function Stack() {
   //用来存储数据
   this.items = []
+}
 
+//定义栈的基本操作
 
-  //定义栈的基本操作
+//1. 把元素压入栈中
+Stack.prototype.push = function (element) {
+  this.items.push(element)
+}
+//2. 从栈中取出元素
+Stack.prototype.pop = function () {
+  return this.items.pop()
+}
 
-  //1. 把元素压入栈中
-  Stack.prototype.push = function (element) {
-    this.items.push(element)
-  }
-  //2. 从栈中取出元素
-  Stack.prototype.pop = function () {
-    return this.items.pop()
-  }
+//3. 查看一下栈顶元素
+Stack.prototype.peek = function () {
+  return this.items[this.items.length - 1]
+}
 
-  //3. 查看一下栈顶元素
-  Stack.prototype.peek = function () {
-    return this.items[this.items.length - 1]
-  }
+//4. 判断栈是否为空
+Stack.prototype.isEmpty = function () {
+  return this.items.length === 0
+}
 
-  //4. 判断栈是否为空
-  Stack.prototype.isEmpty = function () {
-    return this.items === 0
-  }
+//5. 获取栈中的元素个数
+Stack.prototype.size = function () {
+  return this.items.length
+}
 
-  //5. 获取栈中的元素个数
-  Stack.prototype.size = function () {
-    return this.items.length
-  }
+//6. 查看当前栈里面的内容
 
-  //6. 查看当前栈里面的内容
-
-  Stack.prototype.print = function () {
-    return this.items.join(' ')
-  }
+Stack.prototype.print = function () {
+  return this.items.join(' ')
 }
 
 
